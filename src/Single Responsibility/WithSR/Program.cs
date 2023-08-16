6b6d5a767d835160cfbc0e519186286b2bf5d19e
seyed erfan jalili <41292217+ErfanJalili@users.noreply.github.com>
2023-08-16 21:10:13 +0430

    update console

--- src/Single Responsibility/WithSR/Program.cs
+++ src/Single Responsibility/WithSR/Program.cs
@@ -1,6 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using WithSR.Data;
+using WithSR.Models;
+using WithSR.Services;
 
 
-Console.WriteLine("Hello, World!");
+Context Context = new WithSR.Data.Context();
+Car car = new Car();
+
+CarServices carServices = new CarServices(Context);
+
+carServices.CreateCar(car);
+
+
+Console.ReadKey();
